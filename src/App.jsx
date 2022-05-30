7cc16655fa1895d333064263e2c84ec7de1f10c1
DoIfeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2022-05-30 02:53:57 +0100

    arranged the api data into questions and answer. made the question randomly chosen from a list of questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-import {raw} from './util/clean_api.js';
+import {questions as Q, countries, continents} from './util/clean_api.js';
 // import logo from './logo.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(raw)
+  const [qCount, setQCount] = useState({correct: 0, incorrect: 0})
+  const [question, setQuestion] = useState(Q[Math.floor(Math.random() * Q.length)]);
+  const [questions, setQuestions] = useState(Q.filter(q => q != question));
+  console.log(question)
+  console.log(questions)
 
   return (
     <div className="App">
--- src/util/clean_api.js
+++ src/util/clean_api.js
@@ -7,15 +7,59 @@ async function data_fetch(url) {
     return data;
 }
 
+/*
+    Four question categories
+        1. flag
+        2. continent
+        3. currency
+        4.capital
+*/ 
+
 let raw;
 
 if (!localStorage.raw) {
-    raw = await data_fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,continents');
+    raw = await data_fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags,continents');
 
     localStorage.setItem('raw', JSON.stringify(raw));
 }else {
     raw = JSON.parse(localStorage.raw).slice(0,5);
 }
 
+let countries = raw.map(country => {
+    return country.name.common;
+})
 
-export {raw};
+let continents = raw.map(country => {
+    return country.continents[0];
+})
+
+let flag = raw.map(country => {
+    return ({
+        query: country.flags.svg,
+        answer: country.name.common,
+        category: "flag",
+    })
+});
+
+let capital = raw.map(country => {
+    return ({
+        query: country.capital[0],
+        answer: country.name.common,
+        category: "capital",
+    })
+})
+
+let continent = raw.map(country => {
+    return ({
+        query: country.name.common,
+        answer: country.continents[0],
+        category: "continent",
+    })
+})
+
+
+
+let questions = Array.from([].concat(flag, capital, continent));
+questions = questions.filter(question => question.query != undefined);
+
+export {questions, countries, continents};
